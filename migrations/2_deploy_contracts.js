2f0ed25081057e820cff2ce833bb1d2c707a5ada
mindfulme <alice.gogolauri@gmail.com>
2022-10-22 22:36:31 +0300

    minor updates

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@
 const EncantoToken = artifacts.require('./EncantoToken.sol');
 const MasterChef = artifacts.require('./MasterChef.sol');
 const Timelock = artifacts.require('./Timelock.sol');
+const Multicall = artifacts.require('./libs/Multicall.sol');
 module.exports = function (deployer, network, accounts) {
   console.log(network, accounts)
   return deployer.then(() => {
@@ -10,7 +11,8 @@ module.exports = function (deployer, network, accounts) {
 }
 
 async function deployContracts(deployer, network, accounts) {
+  let multiCall = await deployer.deploy(Multicall);
  // let encantoToken = await deployer.deploy(EncantoToken);
   // let masterChef = await deployer.deploy(MasterChef, encantoToken.address, '0x337eE188771A907e6b59d45068A289B27085ac24', '0x337eE188771A907e6b59d45068A289B27085ac24', 1, 1072291);
-  let timelock = await deployer.deploy(Timelock, "0xD0E5a41603C3ec410faD2623B64507C6Cd0141D1", 25200)
+ // let timelock = await deployer.deploy(Timelock, "0xD0E5a41603C3ec410faD2623B64507C6Cd0141D1", 25200)
 }
